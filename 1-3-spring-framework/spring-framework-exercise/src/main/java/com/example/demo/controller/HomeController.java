288353b03fcc8ff0b565d65aee871ce463601ea2
hdxian0984 <jason1233@naver.com>
2025-05-08 21:09:56 +0900

    1-3 스프링 프레임워크의 이해

---
+++ 1-3-spring-framework/spring-framework-exercise/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "메시지 본문입니다.");
+        return "home";
+    }
+}
